5bc0bdd67941fb7e508f10aaafdeb153d2668376
Nishant Rishab <imreprisal@gmail.com>
2022-02-14 20:34:07 +0530

    Splash and dependencies added

---
+++ app/src/main/java/com/example/latticeinovationpvtltd/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.latticeinovationpvtltd
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.databinding.DataBindingUtil
+import com.example.latticeinovationpvtltd.UI.MainActivity
+import com.example.latticeinovationpvtltd.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashScreenBinding
+    lateinit var handler: Handler
+            override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+                binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+                handler = Handler()
+                handler.postDelayed({
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } , 1400)
+    }
+}
